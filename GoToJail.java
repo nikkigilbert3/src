af278bf67bbd2253e82e1460fd3f34a0658dd3e2
thepauloshea <112502733@umail.ucc.ie>
2017-03-06 13:43:10 +0000

    Update GoToJail.java
    added javadoc

--- GoToJail.java
+++ GoToJail.java
@@ -1,14 +1,24 @@
 
 public class GoToJail extends Segment{
-	
+	/**
+	 * Constructor 
+	 *
+	 * @param nme name of player
+	 * @param pos position of player
+	 * 
+	 */
 	public GoToJail(String nme,int pos ){
 		super(pos, nme);
 	}
-	
+	/**
+	 * @param player which player is it
+	 */
 	public void land(Player player){
 		jailed(player);
 	}
-	
+	/**
+	 * @param player puts player in jail
+	 */
 	public void jailed(Player player){
 		player.setBoardPosition(10);
 		player.jailed = true;
