8337461f4785e3171802ed4ae01fb19e97b7eba9
nikkigilbert3 <nikkig626@hotmail.com>
2017-03-06 17:12:38 +0000

    Update Go.java
    added javadoc

--- Go.java
+++ Go.java
@@ -1,9 +1,18 @@
 public class Go extends Segment{
-
+	
+	/**
+	* @param nme name of player
+	* @param pos position of player
+	*/
 	public Go(String nme, int pos) {
 		super(pos, nme);
 	}
 	
+	/**
+	*
+	* @param player when the player lands on GO they will receive 200
+	* 
+	*/
 	public void land(Player player){
 		player.balance.addBalance(200);
 	}
