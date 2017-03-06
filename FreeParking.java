5e6906f1623f6d03929fbc309834a2ed7ac9c6d9
nikkigilbert3 <nikkig626@hotmail.com>
2017-03-06 17:25:56 +0000

    Update FreeParking.java
    added javadoc

--- FreeParking.java
+++ FreeParking.java
@@ -1,16 +1,32 @@
 
 public class FreeParking extends Segment {
 	
+	/**
+	* Constructor
+	* 
+	* @param nme name of the player
+	* @param pos position of the player
+	*
+	*/
 	public FreeParking(String nme, int pos ){
 		super(pos, nme);
 	}
 	
 	static int parked = 1000;
 
+	/**
+	*
+	* @param player player lands on free parking
+	*
+	*/
 	public void land(Player player){
 		freeParking(player);
 	}
 	
+	/**
+	* @param player when the player lands on free parking
+	* they do not owe or receive any money
+	*/
 	public void freeParking(Player player){
 		player.balance.addBalance(parked);
 		parked = 0;
